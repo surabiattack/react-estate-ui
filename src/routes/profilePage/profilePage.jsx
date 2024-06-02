6e9c2a4f685abc2d9c4dd683d36b0085eff7da97
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2024-06-02 21:10:32 +0700

    React.js Chat Component Design

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,3 +1,4 @@
+import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
 
@@ -37,7 +38,9 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <Chat />
+        </div>
       </div>
     </div>
   );
