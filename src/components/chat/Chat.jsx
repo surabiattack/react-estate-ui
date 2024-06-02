c1ad9c19e9c3f3cbcb41fa51710e2ef25019f109
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2024-06-02 21:41:32 +0700

    React Messenger Chat Window Design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./chat.scss";
 function Chat() {
+  const [chat, setChat] = useState(true);
+
   return (
     <div className="chat">
       <div className="messages">
@@ -45,7 +48,60 @@ function Chat() {
           <p>Lorem ipsum dolor sit amet..</p>
         </div>
       </div>
-      <div className="chatBox">box</div>
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              John Doe
+            </div>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
+          </div>
+          <div className="center">
+            <div className="chatMessage">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum, dolor sit</p>
+              <span>1 Hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea name="" id=""></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
