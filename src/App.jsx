b84a242f352d76f96efb8be5dd099116b98faaf6
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2024-06-02 20:43:07 +0700

    React.js Responsive Profile Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./routes/homePage/homePage.jsx";
 import ListPage from "./routes/listPage/listPage.jsx";
 import Layout from "./routes/layout/layout.jsx";
 import SinglePage from "./routes/singlePage/singlePage.jsx";
+import ProfilePage from "./routes/profilePage/profilePage.jsx";
 
 function App() {
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
         {
-            path: "/",
-            element: <Layout/>,
-            children: [
-                {
-                    path: "/",
-                    element: <HomePage/>
-                },
-                {
-                    path: "/list",
-                    element: <ListPage/>
-                },
-                {
-                    path: "/:id",
-                    element: <SinglePage/>
-                },
-            ]
+          path: "/",
+          element: <HomePage />,
         },
-    ])
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
 
-    return (
-        <RouterProvider router={router}/>
-    )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
